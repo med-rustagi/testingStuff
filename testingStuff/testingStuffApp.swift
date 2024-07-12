d2b7acff1cbfe233ab93094db22e0990b50c1bc4
Medha Rustagi <an_rustagi@icloud.com>
2024-07-12 13:32:13 -0700

    Initial Commit

---
+++ testingStuff/testingStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingStuffApp.swift
+//  testingStuff
+//
+//  Created by Medha on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct testingStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
